ac6ffbfaa35f927fa86a8958e1cc1da612fca7d7
Andreas Wolf <dev@a-w.io>
2024-04-07 17:24:22 +0200

    Add PHPStan

--- tests/Configuration/LifterConfigTest.php
+++ tests/Configuration/LifterConfigTest.php
@@ -6,6 +6,9 @@ use a9f\Lifter\Configuration\LifterConfig;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 
+use function Safe\getcwd;
+use function Safe\chdir;
+
 final class LifterConfigTest extends TestCase
 {
     #[Test]
