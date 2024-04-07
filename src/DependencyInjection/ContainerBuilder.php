1aee7e4d69f62b142142197b0501a0ecf2ebb2b3
Andreas Wolf <dev@a-w.io>
2024-04-07 13:39:59 +0200

    Add compiler pass for CLI commands

--- src/DependencyInjection/ContainerBuilder.php
+++ src/DependencyInjection/ContainerBuilder.php
@@ -2,6 +2,7 @@
 
 namespace a9f\Lifter\DependencyInjection;
 
+use a9f\Lifter\DependencyInjection\CompilerPass\CommandsCompilerPass;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder as SymfonyContainerBuilder;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -12,8 +13,12 @@ final class ContainerBuilder
     public function build(): ContainerInterface
     {
         $containerBuilder = new SymfonyContainerBuilder();
+        $containerBuilder->addCompilerPass(new CommandsCompilerPass());
+
         $loader = new YamlFileLoader($containerBuilder, new FileLocator(__DIR__ . '/../../config/'));
         $loader->load('services.yaml');
+
+        $containerBuilder->compile();
 
         return $containerBuilder;
     }
